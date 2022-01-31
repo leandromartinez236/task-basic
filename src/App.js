f53ac8f2117cfc5767e901034ef8975b13d71dd2
leandro <martinezleadro2004@gmail.com>
2022-01-31 20:53:24 -0300

    resuelto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [task, setTask] = useState([]);
+  const [valor, setValor] = useState('');
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Task Basic</h1>
+      <form id='form'>
+        <div>
+          <label className='marginbottom'>Enter a Task</label>
+          <input className='marginbottom' type='text' placeholder='Ingrese una tarea' value={valor} onChange={(e)=> setValor (e.target.value)}/> 
+        </div>
+        <button type='button' onClick={()=> setTask([...task, valor])}>ADD TASK</button>
+        {task.map((e) => <div key={e} className='task'>{e}</div>  )}       
+      </form>
     </div>
   );
 }
