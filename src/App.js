29d4fd983dc6287246b4798fa39447c723274141
leandro <martinezleadro2004@gmail.com>
2022-02-07 00:39:34 -0300

    useEffect implementado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import './App.css'
 
-function App() {
-  const [task, setTask] = useState([]);
-  const [valor, setValor] = useState('');
-  
-  return (
-    <div className="App">
-      <h1>Task Basic</h1>
-      <form id='form'>
+function App (){
+  const [task, setTask] = useState('');
+  const [listTasks, setListTasks] = useState([]);
+  useEffect(()=>{
+    setTask('')
+  },[listTasks])
+  return(
+    <div className='App'>
+      <h1>Task basic</h1>
+      <form >
         <div>
           <label className='marginbottom'>Enter a Task</label>
-          <input className='marginbottom' type='text' placeholder='Ingrese una tarea' value={valor} onChange={(e)=> setValor (e.target.value)}/> 
+          <input className='marginbottom' type="text" placeholder='Enter a task' 
+          value={task} onChange={(e)=> setTask(e.target.value)}/>
         </div>
-        <button type='button' onClick={()=> setTask([...task, valor])}>ADD TASK</button>
-        {task.map((e) => <div key={e} className='task'>{e}</div>  )}       
+        <button type='button' onClick={()=> setListTasks([...listTasks, task])}>ADD TASK</button>
+      {listTasks.map((task)=> <div className='task' key={task}>{task}</div>)}
       </form>
     </div>
   );
 }
-
 export default App;
